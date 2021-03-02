398ba9cbda0affe9406e049289b1650b110f9c8d
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 16:17:00 +0200

    index: attached the dotenv into index

--- public/index.php
+++ public/index.php
@@ -6,9 +6,18 @@ use app\core\Application;
 use app\controller\PagesController;
 use app\controller\UserController;
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
+$config =[
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
 
-$app = new Application(dirname(__DIR__));
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [PagesController::class, 'home']);
 
