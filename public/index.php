e64109d6a0fb82fb1e9c4eb7ef440c25839f175e
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 15:33:38 +0200

    index: created register and login routes both post and get methods are working

--- public/index.php
+++ public/index.php
@@ -15,6 +15,10 @@ $app->router->get('/', [PagesController::class, 'home']);
 $app->router->get('feedback', [PagesController::class, 'feedback']);
 
 $app->router->get('register', [UserController::class, 'register']);
+$app->router->post('register', [UserController::class, 'register']);
+
+$app->router->get('login', [UserController::class, 'login']);
+$app->router->post('login', [UserController::class, 'login']);
 
 
 
