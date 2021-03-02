9fb71d31e4ee503528a563fc612fa67c30cd7379
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 16:44:31 +0200

    usermodel: somce fixes with binding parameters

--- model/UserModel.php
+++ model/UserModel.php
@@ -42,13 +42,16 @@ class UserModel
     public function register($data)
     {
         // prepare statment
-        $this->db->query("INSERT INTO users (`name`, `email`, `password`) VALUES (:name, :email, :password)");
+        $this->db->query("INSERT INTO users (`name`, `surname`, `email`, `password`, `phone`, `address`) VALUES (:name, :surname, :email, :password, :phone, :address)");
 
         // add values
         $this->db->bind(':name', $data['name']);
+        $this->db->bind(':surname', $data['surname']);
         $this->db->bind(':email', $data['email']);
         // hasshed
         $this->db->bind(':password', $data['password']);
+        $this->db->bind(':phone', $data['phone']);
+        $this->db->bind(':address', $data['address']);
 
         // make query
         if ($this->db->execute()) {
