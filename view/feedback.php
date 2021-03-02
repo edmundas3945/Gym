9cb096e633cb8e74324bd3e1021c6beb9c1529e8
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 20:00:40 +0200

    feedback: modified feedback to loged users and not loged users

--- view/feedback.php
+++ view/feedback.php
@@ -1,2 +1,10 @@
-<?php echo $title ?>
-<?php echo $description ?>
+<h3><strong>Here is feedback about us:</strong></h3>
+
+<?php if (!\app\core\Session::isUserLoggedIn()) : ?>
+    <h3>Wanna leave a feedback? go <a href="/login">Login</a></h3>
+<?php else : ?>
+    <form class="form-group" action="" method="post">
+        <textarea class="form-group" name="comment" id="comment" cols="80" rows="3"></textarea>
+        <button class="btn btn-primary btn-block" >Submit</button>
+    </form>
+<?php endif; ?>
