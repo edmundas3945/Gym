4262431da45ca72645979e7003098afd6c6b6150
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 23:33:10 +0200

    feedbackController: add vld and comments from sql

--- controller/FeedbackController.php
+++ controller/FeedbackController.php
@@ -5,23 +5,27 @@ namespace app\controller;
 use app\core\Controller;
 use app\core\Request;
 use app\model\FeedbackModel;
+use app\controller\Validation;
 
 class FeedbackController extends Controller
 {
+    public Validation $vld;
     protected FeedbackModel $feedbackModel;
 
     public function __construct()
     {
         $this->feedbackModel = new FeedbackModel();
+        $this->vld = new Validation();
     }
     public function feedback(Request $request)
     {
 
         if ($request->isGet()) {
-            $data = [
-                'comment' => 'sudas',
-            ];
-            echo $data['comment'];
+            
+            $data['comments'] = $this->feedbackModel->getFeedback();
+            // echo '<pre>';
+            // print_r($data['comments']);
+            // echo '</pre>';
             return $this->render('/feedback', $data);
         }
 
@@ -29,15 +33,17 @@ class FeedbackController extends Controller
         if ($request->isPost()) {
             // var_dump($_POST['comment']);
             // var_dump($_SESSION['user_name']);
-            $data = [
-                'comment' => $_POST['comment'],
-                'name' => $_SESSION['user_name'],
+            $data = $request->getBody();
+            $data['comments'] = $this->feedbackModel->getFeedback();
+            // echo '<pre>';
+            // var_dump($data);
+            // echo '</pre>';
 
-            ];
-            
-            // if (strlen($data['comment']) < 400 ){
-            //     // echo 'swx';
-            // }
+            $data['commentErr'] = $this->vld->validateComment($data['comment']);
+
+            if (empty($data['commentErr'])){
+                $this->feedbackModel->createComment($data);
+            }
 
             return $this->render('/feedback', $data);
         }
