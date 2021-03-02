0eb59440878c23f0610954758baf6ad762563fd1
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 15:35:15 +0200

    main: changed map load from lazy to fast

--- view/layout/main.php
+++ view/layout/main.php
@@ -38,7 +38,7 @@
       
     </div>
 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.219426156724!2d25.3356966160162!3d54.72335507837753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0xdd7887e198efd4c7!2sSaul%C4%97tekio%20al.%2015%2C%20Vilnius%2010224!5e0!3m2!1sen!2slt!4v1614672592134!5m2!1sen!2slt" width="100%" height="300" style="border:0;" class="map" allowfullscreen="" loading="lazy"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.219426156724!2d25.3356966160162!3d54.72335507837753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0xdd7887e198efd4c7!2sSaul%C4%97tekio%20al.%2015%2C%20Vilnius%2010224!5e0!3m2!1sen!2slt!4v1614672592134!5m2!1sen!2slt" width="100%" height="300" style="border:0;" class="map" allowfullscreen="" loading="fast"></iframe>
     <footer class=" text-center text-lg-start">
       <div class="text-center p-3" >
         © 2021 <a class="text-dark" href="https://www.linkedin.com/in/edmundas-gataveckas-69148816a/">Edmundas Gataveckas</a>, all rights reserved.
