772083218b6c2a20899d2b71e048d1a27b0db0a1
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 14:53:53 +0200

    auth: rewrite auth layout

---
+++ view/layout/auth.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../public/css/style.css">
+  <title>Your One And Only Gym</title>
+</head>
+
+<body>
+  <div class="page-container">
+    <div class="page-content-auth">
+      <nav class="navbar navbar-expand-lg navbar-light bg-c">
+        <div class="container">
+          <a class="navbar-brand text-dark" href="/">Lemon<strong>Gym</strong></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link text-dark" href="/feedback">Feedback</a>
+            </div>
+            <div class="navbar-nav ml-auto">
+              <a class="nav-link text-dark" href="/login">Login</a>
+              <a class="nav-link text-dark" href="/register">Register</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container">
+        {{content}}
+      </div>
+      
+    </div>
+
+    <footer class=" text-center text-lg-start">
+      <div class="text-center p-3" >
+        © 2021 <a class="text-dark" href="https://www.linkedin.com/in/edmundas-gataveckas-69148816a/">Edmundas Gataveckas</a>, all rights reserved.
+      </div>
+    </footer>
+
+  </div>
+
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</html>
