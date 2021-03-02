72b2bdcfe5076da38c65ac4cf93908c4c72b3453
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 18:21:41 +0200

    main: made nav bar adaptive to loged in user or not loged in

--- view/layout/main.php
+++ view/layout/main.php
@@ -23,24 +23,32 @@
             <div class="navbar-nav">
               <a class="nav-link text-dark" href="/feedback">Feedback</a>
             </div>
-            <div class="navbar-nav ml-auto">
-              <a class="nav-link text-dark" href="/login">Login</a>
-              <a class="nav-link text-dark" href="/register">Register</a>
-            </div>
+            <?php if (!\app\core\Session::isUserLoggedIn()) : ?>
+              <div class="navbar-nav ml-auto">
+                <a class="nav-link text-dark" href="/login">Login</a>
+                <a class="nav-link text-dark" href="/register">Register</a>
+              </div>
+            <?php else : ?>
+              <div class="navbar-nav ml-auto ">
+                <a class="nav-link disabled" href="#"><?php echo $_SESSION['user_email'] ?></a>
+                <a class="nav-link" href="/logout">Logout</a>
+              </div>
+            <?php endif; ?>
           </div>
         </div>
+
       </nav>
       <div class="topImage"></div>
 
       <div class="container">
         {{content}}
       </div>
-      
+
     </div>
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.219426156724!2d25.3356966160162!3d54.72335507837753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0xdd7887e198efd4c7!2sSaul%C4%97tekio%20al.%2015%2C%20Vilnius%2010224!5e0!3m2!1sen!2slt!4v1614672592134!5m2!1sen!2slt" width="100%" height="300" style="border:0;" class="map" allowfullscreen="" loading="fast"></iframe>
     <footer class=" text-center text-lg-start">
-      <div class="text-center p-3" >
+      <div class="text-center p-3">
         © 2021 <a class="text-dark" href="https://www.linkedin.com/in/edmundas-gataveckas-69148816a/">Edmundas Gataveckas</a>, all rights reserved.
       </div>
     </footer>
