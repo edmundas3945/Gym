86c077472ab469999e2d9315dd4ce3f69ad617f9
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-03 00:12:34 +0200

    feedback: working comments with php, enaught  with that

--- view/feedback.php
+++ view/feedback.php
@@ -1,10 +1,33 @@
-<h3><strong>Here is feedback about us:</strong></h3>
+<h3 class="feedbackTitle mt-3 mb-3"><strong>Here is feedback about us:</strong></h3>
+
+<table style="width:100%" class="comments">
+  <tr>
+    <th style="width:150px">User Name</th>
+    <th style="width:auto">Comment</th>
+  </tr>
+  <?php foreach ($comments as $item) :?>
+  <tr>
+    <td><?php echo $item->name ?></td>
+    <td><?php echo $item->comment ?></td>
+  </tr>
+  <?php endforeach ?>
+</table>
+
+<div class="comments"></div>
 
 <?php if (!\app\core\Session::isUserLoggedIn()) : ?>
-    <h3>Wanna leave a feedback? go <a href="/login">Login</a></h3>
+    <h3 class="mt-5 mb-5">Wanna leave a feedback? go <a href="/login">Login</a></h3>
 <?php else : ?>
-    <form class="form-group" action="" method="post">
-        <textarea class="form-group" name="comment" id="comment" cols="80" rows="3"></textarea>
-        <button class="btn btn-primary btn-block" >Submit</button>
-    </form>
+    <form class="form-group mt-3" action="" id="commentForm" method="post">
+        <textarea class="mb-0 <?php echo (!empty($commentErr)) ? 'is-invalid' : ''; ?> form-control form-group form-control-lg" name="comment" id="comment" cols="80" rows="3"></textarea>
+        <span class='invalid-feedback'><?php echo $commentErr ?></span>
+        <input type="submit" value="Submit" class="btn mt-3 btn-primary btn-block">
+</form>
 <?php endif; ?>
+
+<script>
+  const commentsOutput = document.getElementById('comments')
+
+</script>
+
+
