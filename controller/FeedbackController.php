a6c90f6c254c12c5ae51d444c6470fe2875201d0
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2021-03-02 22:25:30 +0200

    feedbackController: trying to do somthing but shit not working

--- controller/FeedbackController.php
+++ controller/FeedbackController.php
@@ -3,17 +3,43 @@
 namespace app\controller;
 
 use app\core\Controller;
+use app\core\Request;
+use app\model\FeedbackModel;
 
 class FeedbackController extends Controller
 {
+    protected FeedbackModel $feedbackModel;
 
-    public function feedback()
+    public function __construct()
     {
-        $data = [
-            'title' => 'This is your favorite gym\'s feedback',
-            'description' => 'Tell me about your saiyan powers'
-        ];
+        $this->feedbackModel = new FeedbackModel();
+    }
+    public function feedback(Request $request)
+    {
 
-        return $this->render('/feedback', $data);
+        if ($request->isGet()) {
+            $data = [
+                'comment' => 'sudas',
+            ];
+            echo $data['comment'];
+            return $this->render('/feedback', $data);
+        }
+
+
+        if ($request->isPost()) {
+            // var_dump($_POST['comment']);
+            // var_dump($_SESSION['user_name']);
+            $data = [
+                'comment' => $_POST['comment'],
+                'name' => $_SESSION['user_name'],
+
+            ];
+            
+            // if (strlen($data['comment']) < 400 ){
+            //     // echo 'swx';
+            // }
+
+            return $this->render('/feedback', $data);
+        }
     }
 }
